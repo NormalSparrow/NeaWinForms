8bead4d20f12ffd5cade7460bbf7426cb61b5bba
Kaden Sparrow <kaden.sparrow@gmail.com>
2025-02-19 15:35:49 +0000

    Mixing channels work

--- NEA Audio GUI/Form1.cs
+++ NEA Audio GUI/Form1.cs
@@ -1,5 +1,6 @@
 
 using NAudio.Wave;
+using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace NEA_Audio_GUI
@@ -48,42 +49,50 @@ namespace NEA_Audio_GUI
                 audioStream = null;
             }
 
-          List<RawSourceWaveStream> streams = new List<RawSourceWaveStream>();
-            foreach (var wavetype in inUseWaveTypes)
+            List<RawSourceWaveStream> streams = new List<RawSourceWaveStream>();
+            foreach (var waveType in inUseWaveTypes)
             {
+                RawSourceWaveStream stream = null;
                 switch (waveType)
                 {
                     case WaveType.Karplus:
-                        audioStream = audioKarplus.Decay(frequency: frequency);
+                        stream = audioKarplus.Decay(frequency: frequency);
                         break;
 
                     case WaveType.Triangle:
-                        audioStream = audioTriangle.Triangle(frequency: frequency);
+                        stream = audioTriangle.Triangle(frequency: frequency);
                         break;
-
-                    default:
-                        MessageBox.Show("Please toggle one or more wavetypes to generate sound.");
-                        return; //break case
                 }
-                if (streams.Count > 0)
+
+                if (stream != null)
                 {
-                    //foreach (var stream in streams) {
-                    //  List<byte[]> sampleBuffers = new List<byte[]>();}
-                    audioStream = MixStreams(streams.ToArray());
+                    streams.Add(stream);
                 }
             }
-       
+
+      
+            if (streams.Count > 0)
+            {
+                audioStream = MixStreams(streams.ToArray());
+            }
+
+           
             if (audioStream != null)
             {
                 await audioPlayer.PlayAudio(audioStream);
             }
             else
             {
-                MessageBox.Show("No audio stream generated.");
+                MessageBox.Show("No audio stream generated. Please toggle one or more wave types.");
             }
         }
+
         private RawSourceWaveStream MixStreams(RawSourceWaveStream[] streams)
         {
+            if(streams.Length == 0)
+            {
+                throw new ArgumentException("something has gone incredibly wrong, mixstreams is being called with no streams");
+            }
             var format = streams[0].WaveFormat;
             foreach (var stream in streams)
             {
@@ -98,7 +107,7 @@ namespace NEA_Audio_GUI
                 byte[] buffer = new byte[stream.Length];
                 stream.Read(buffer, 0, buffer.Length); //read samples to buffer
                 sampleBuffers.Add(buffer);   
-            }
+            }   
             List<short> mixedSamples = new List<short>();
             for (int i = 0; i < sampleBuffers[0].Length; i += 2) // Process 2 bytes at a time (16-bit samples)
             {
@@ -132,11 +141,11 @@ namespace NEA_Audio_GUI
         private void decayButton_Click(object sender, EventArgs e)
         {
 
-            waveType = WaveType.Karplus;
+            toggleButton(WaveType.Karplus);
         }
         private void triangleWaveButton_Click(object sender, EventArgs e)
         {
-            waveType = WaveType.Triangle;
+          toggleButton(WaveType.Triangle);
         }
         private void toggleButton (WaveType waveType)
         {
@@ -152,8 +161,25 @@ namespace NEA_Audio_GUI
         }
         private void ButtonAppearance()
         {
-        //    decayButton.BackColor = inUseWaveTypes.Contains(WaveType.Karplus) ? Color.Green : SystemColors.Control;
-          //  triangleWaveButton.BackColor = inUseWaveTypes.Contains(WaveType.Triangle) ? Color.Green : SystemColors.Control; make dictionary
+            //    decayButton.BackColor = inUseWaveTypes.Contains(WaveType.Karplus) ? Color.Green : SystemColors.Control;
+            //  triangleWaveButton.BackColor = inUseWaveTypes.Contains(WaveType.Triangle) ? Color.Green : SystemColors.Control; make dictionary
+
+
+            foreach (var keyValuePair in waveTypeButtons)
+            {
+                WaveType waveType = keyValuePair.Key;
+                Button button = keyValuePair.Value;
+
+                if (inUseWaveTypes.Contains(waveType))
+                {
+                    button.BackColor = Color.Green;
+                }
+                else
+                {
+                    button.BackColor = Color.White;
+                }
+            
+            }
         }
         private void Volume_Scroll(object sender, EventArgs e)
         {
